70173478d8e96cc5405d9171b361d9f341724ab0
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2017-01-20 14:29:03 -0500

    move header styles into Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
-import {header} from '../styles/common'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import React, {PropTypes} from 'react'
 
+const theme = getMuiTheme()
+
+const style = {
+  background: theme.palette.primary1Color,
+  color: theme.palette.alternateTextColor,
+  margin: 0,
+  padding: '2rem 0 .5rem'
+}
+
 const Header = ({children}) => (
-  <h1 style={header}>{children}</h1>
+  <h1 style={style}>{children}</h1>
 )
 
 Header.propTypes = {
--- src/styles/common.js
+++ src/styles/common.js
@@ -1,10 +1,10 @@
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-
-const theme = getMuiTheme()
-
-export const header = {
-  background: theme.palette.primary1Color,
-  color: theme.palette.alternateTextColor,
-  margin: 0,
-  padding: '2rem 0 .5rem'
-}
+// import getMuiTheme from 'material-ui/styles/getMuiTheme'
+//
+// const theme = getMuiTheme()
+//
+// export const header = {
+//   background: theme.palette.primary1Color,
+//   color: theme.palette.alternateTextColor,
+//   margin: 0,
+//   padding: '2rem 0 .5rem'
+// }
